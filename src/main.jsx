08d97aa4136bf62ccd179c6fcb24259ca6b91b73
Jyotish Kumar <jyotishgehlot@gmail.com>
2026-07-22 12:58:19 +0530

    feat(contact):create a toaster for notification

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 import "./styles/index.css";
 import App from "./App.jsx";
@@ -9,6 +10,19 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Toaster
+        position="bottom-right"
+        gutter={12}
+        toastOptions={{
+          duration: 4000,
+          style: {
+            background: "transparent",
+            boxShadow: "none",
+            padding: 0,
+            border: "none",
+          },
+        }}
+      />
     </BrowserRouter>
   </StrictMode>,
 );
